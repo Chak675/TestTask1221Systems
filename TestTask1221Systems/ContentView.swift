86e4cf349eeaded46fb3bb23690681a6e239a0e4
Dmitrii Zorin <zchakiz@gmail.com>
2024-08-07 14:38:28 +0300

    Initial Commit

---
+++ TestTask1221Systems/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestTask1221Systems
+//
+//  Created by Zorin Dmitrii on 07.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestTask1221Systems/TestTask1221SystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTask1221SystemsApp.swift
+//  TestTask1221Systems
+//
+//  Created by Zorin Dmitrii on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestTask1221SystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
