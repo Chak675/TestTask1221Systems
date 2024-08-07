0409466b9eb4a24eaf0b4bd9ac58a008c7297d8a
Dmitrii Zorin <zchakiz@gmail.com>
2024-08-07 14:54:59 +0300

    add model

---
+++ TestTask1221Systems/Models/ProductModel.swift
@@ -0,0 +1,17 @@
+//
+//  ProductModel.swift
+//  TestTask1221Systems
+//
+//  Created by Zorin Dmitrii on 07.08.2024.
+//
+
+import Foundation
+
+struct ProductModel {
+    
+    let id: Int
+    let name: String
+    let rate: Double
+    let image: URL
+    let price: Double
+}
